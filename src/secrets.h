51715eae5f2a03f9b9d176880ce663a7203c50d9
CeC <cecatlett@gmail.com>
2021-07-21 17:56:59 -0500

    oops

--- src/secrets.h
+++
@@ -1,5 +0,0 @@
-const char *HA_USR = "Electronmqtt";
-const char *HA_PWD = "Cabin2021mqtt";
-char MY_SERVER[] = "hassio.deepblueberry.com";
-//byte MY_SERVER[] = { 73, 247, 85, 17 };
-const char *CLIENT_NAME = "CABIN"; // must be unique per device!
